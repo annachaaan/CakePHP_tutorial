274daa2c7e0e60808aede41b308f9c03b730c3b6
郷田杏奈 <a_gouda@funteam.co.jp>
2019-02-05 15:12:36 +0900

    投稿された記事の詳細を表示するviewページ作成(アクションとビュー)

--- cakephp/app/Controller/PostsController.php
+++ cakephp/app/Controller/PostsController.php
@@ -3,6 +3,20 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
     public function index() {
+        // set() を使って、コントローラからビューにデータを渡す
         $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        // 一つの投稿記事を取得するので、afindByID()を使用
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Indalid post'));
+        }
+        $this->set('post', $post);
     }
 }
