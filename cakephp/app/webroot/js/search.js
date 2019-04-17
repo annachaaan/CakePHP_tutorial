97173edf82c8384db662b7cffee916e855d9d064
annachaaaaaan <anna_chaaaaaan.g@gmail.com>
2019-04-17 18:15:44 +0900

    一意でない郵便番号入力時の対応

--- cakephp/app/webroot/js/search.js
+++ cakephp/app/webroot/js/search.js
@@ -15,15 +15,38 @@ $(function(){
                     // Ajaxリクエストが成功した時発動
                     .done( (data) => {
                         json = JSON.parse(data);
+                        json_length = json.length;
                         // jsonデータがある時のみ値を返す
                         if (json[0]) {
-                            pref = json[0].postelcodes.pref;
-                            city = json[0].postelcodes.city;
-                            street = json[0].postelcodes.street;
+                            if (json_length == 1) {
+                                pref = json[0].postelcodes.pref;
+                                city = json[0].postelcodes.city;
+                                street = json[0].postelcodes.street;
 
-                            $('#adress').val(pref + city + street);
+                                $('#adress').val(pref + city + street);
+                            } else {
+                                list = [];
+                                for (var i = 0; i < json_length; i++) {
+                                    pS = '<p>';
+                                    pF = '</p>';
+                                    selectS = '<datalist id="select" name="data[User][adress_auto]" class="form-control">'
+                                    selectF = '</datalist>';
+                                    tagS = '<option value =';
+                                    tagE = '>'
+                                    tagF = '</option>'
+                                    list[i] = tagS + json[i].postelcodes.pref + json[i].postelcodes.city + json[i].postelcodes.street + tagE;
+                                }
+                                console.log(list);
+                                pref = json[0].postelcodes.pref;
+                                city = json[0].postelcodes.city;
+
+                                // 配列の区切り文字を削除
+                                list = list.join("");
+                                // プルダウン作成
+                                $('#adress').replaceWith('<input id="adress" list="select" autocomplete="on" type="text" name="data[User][adress_auto]" class="form-control"><datalist id="select">' + list + '</datalist>');
+                                $('#adress').val(pref + city);
+                            }
                         }
-                        // console.log(json[0]);
                     })
                     // Ajaxリクエストが失敗した時発動
                     .fail( (data) => {
